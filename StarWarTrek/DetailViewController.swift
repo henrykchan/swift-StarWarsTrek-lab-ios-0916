9443ee0c5b8f594355730411a3d275bbfba117e9
henrykchan <chan.henryk@gmail.com>
2016-10-20 13:26:04 -0400

    Finish

---
+++ StarWarTrek/DetailViewController.swift
@@ -0,0 +1,62 @@
+//
+//  DetailViewController.swift
+//  StarWarTrek
+//
+//  Created by Henry Chan on 10/19/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    
+    @IBOutlet weak var starWarsImageView: UIImageView!
+    @IBOutlet weak var starTrekImageView: UIImageView!
+    
+    var starWarsImage: UIImage!
+    var starTrekImage: UIImage!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        starTrekImageView.image = starTrekImage
+        starWarsImageView.image = starWarsImage
+        
+        starWarsPanGesture()
+        starTrekPanGesture()
+        
+    }
+
+   
+    func starWarsPanGesture() {
+        
+        let starWarsPan = UIPanGestureRecognizer(target: self, action: #selector(draggingView))
+        
+        starWarsImageView.addGestureRecognizer(starWarsPan)
+    }
+    
+    
+    func starTrekPanGesture() {
+        
+        let starTrekPan = UIPanGestureRecognizer(target: self, action: #selector(draggingView))
+        
+        starTrekImageView.addGestureRecognizer(starTrekPan)
+    }
+    
+    
+    
+    
+    func draggingView(_ sender: UITapGestureRecognizer) {
+        
+        
+        let point = sender.location(in: view)
+        let draggedView = sender.view as! UIImageView
+        draggedView.center = point
+        print("Draggggggggging")
+        
+    }
+
+   
+
+}
--- StarWarTrek/ViewController.swift
+++ StarWarTrek/ViewController.swift
@@ -10,10 +10,110 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var anakinImage: UIImageView!
+    @IBOutlet weak var chewbaccaImage: UIImageView!
+    @IBOutlet weak var hanSoloImage: UIImageView!
+    @IBOutlet weak var lukeSkyImage: UIImageView!
+    
+    @IBOutlet weak var captainKirkImage: UIImageView!
+    @IBOutlet weak var drMcCoyImage: UIImageView!
+    @IBOutlet weak var spockImage: UIImageView!
+    @IBOutlet weak var uhuruImage: UIImageView!
+    
+    
+    var starWarsCharacter: UIImageView!
+    var starTrekCharacter: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        starWarsGestureRecognizers()
+        starTrekGestureRecognizers()
+        
     }
+    
+
+    
+    func starWarsGestureRecognizers() {
+        
+        let starWarsImage  = [anakinImage, chewbaccaImage, hanSoloImage, lukeSkyImage]
+        
+        for image in starWarsImage {
+            let starWarsGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(starWarsCharacterChosen))
+            starWarsGestureRecognizer.numberOfTapsRequired = 1
+            image?.addGestureRecognizer(starWarsGestureRecognizer)
+        }
+    }
+    
+    
+    func starTrekGestureRecognizers() {
+        
+        let starTrekImage = [captainKirkImage, drMcCoyImage, spockImage, uhuruImage]
+        
+        for image in starTrekImage {
+            let starTrekGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(starTrekCharacterChosen))
+            
+            starTrekGestureRecognizer.numberOfTapsRequired = 1
+            image?.addGestureRecognizer(starTrekGestureRecognizer)
+        }
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "fightSegue" {
+            
+            let detailView = segue.destination as! DetailViewController
+            detailView.starWarsImage = starWarsCharacter.image
+            detailView.starTrekImage = starTrekCharacter.image
+            print("hewiofjiwjfioweiofjow")
+        }
+        
+    }
+    
+    
+    
+    func starWarsCharacterChosen(_ sender: UITapGestureRecognizer) {
+        
+        if starWarsCharacter != nil { starWarsCharacter.layer.borderWidth = 0.0 }
+        let chosenImageView = sender.view as! UIImageView
+        chosenImageView.layer.borderWidth = 2.0
+        chosenImageView.layer.borderColor = UIColor.green.cgColor
+        starWarsCharacter = chosenImageView
+        checkForFight()
+        print ("StarWARSSSSSSS")
+    }
+    
+    
+    func starTrekCharacterChosen(_ sender: UITapGestureRecognizer) {
+        
+        if starTrekCharacter != nil { starTrekCharacter.layer.borderWidth = 0.0 }
+        let chosenImageView = sender.view as! UIImageView
+        chosenImageView.layer.borderWidth = 2.0
+        chosenImageView.layer.borderColor = UIColor.green.cgColor
+        starTrekCharacter = chosenImageView
+        checkForFight()
+        print ("Star TREKKKKKK")
+        
+    }
+    
+    
+    func checkForFight() {
+        
+        if starWarsCharacter != nil && starTrekCharacter != nil {
+            
+            performSegue(withIdentifier: "fightSegue", sender: nil)
+        }
+    }
+    
+    func panGesture() {
+        
+        
+    }
+    
+    
+    
+    
     
  
 }
